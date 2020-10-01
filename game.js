9db5991ee8f99220b672351fdf34dab8b7f45aeb
Wenlong Yan <34890088+WenlongYan131@users.noreply.github.com>
2020-10-01 03:09:02 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -2,8 +2,6 @@
 var highest_score = getCookie("highest_score");
 var rounds = getCookie("rounds");
 
-
-
 $(document).ready(function (){
     var level = 1;
     var width = 100;
@@ -11,11 +9,16 @@ $(document).ready(function (){
     var num = 0;
     var score = 0;
     var stop = true;
+    var running = false;
     var $play = $(".play");
+
+    build();
+    update();
 
     function update(){
         $("#highest").html("Highest Score: "+highest_score);
         $("#rounds").html("Rounds you have played: "+rounds);
+        $("#level").html("Level: "+level);
         $("#score").html("Your Score: "+score);
     }
 
@@ -24,28 +27,30 @@ $(document).ready(function (){
     function build(){
         $(".platform").append('<div class="new_platform" style="width:'+width+'px ; left:0;background: darkgray"></div>');
         setTimeout(function () {
-            var max=$(".main").offset().top;
-            var min=50;
+            var max = $(".main").offset().top;
+            var min = 50;
 
             for(var i=0;i<100;i++){
                 var next=$(".new_platform").eq(i).offset().left;
                 var sum = next+width+parseInt(Math.random()*(max-min))+min;
-                $(".platform").append('<div class="new_platform" style="width:'+w+'px ; left:'+sum+'px;;background: darkgray"></div>');
-                if((i+1)%5===0){
-                    if(w>10)
-                        w-=10;
+                $(".platform").append('<div class="new_platform" style="width:'+width+'px ; left:'+sum+'px;background: darkgray"></div>');
+                if((i+1)%3===0){
+                    if(width>20)
+                        width-=20;
                 }
             }
         },50)
     }
 
-    build();
-    update();
 
     $play.mousedown(function () {
+        if(running===true)
+            return ;
         $(".play").addClass("playDown");
         if(stop === true){
             var max=$(".main").offset().top;
+            var w = $(".new_platform").eq(num).width();
+            $(".rod").css("left",w);
             $(".rod").animate({"width":max+"px"},1000);
 
         }
@@ -68,7 +73,9 @@ $(document).ready(function (){
     // });
 
     $("#help").click(function(){
-        alert("Hold the button to start making the bridge longer and release to stop, try to make the Match man reach the next platform and not falling down");
+        alert("Hold the red button to start making the bridge longer and release to stop," +
+            " try to make the Match Man reach the next platform and not falling down." +
+            " The platform will become shorter as the level improve.");
     });
 
     $("#clear").click(function(){
@@ -76,31 +83,38 @@ $(document).ready(function (){
     });
 
     function run(){
+        running = true;
         var len = $(".rod").width();
         setTimeout(function () {
-            $(".man").find("img").attr("src","imgs/run.gif");
-            $(".man").find("img").animate({"left":(len+width)+"px"},1000,function () {
-                var dist = $(".new_platform").eq(num+1).offset().left-width;
-                if((len+20)<dist||len>dist+width){
-                    //console.log("0");
+            var w = $(".new_platform").eq(num+1).width();
+            console.log(w);
+            $(".man").find("img").attr("src","imgs/running.gif");
+            $(".man").find("img").animate({"left":(len+w)+"px"},1000,function () {
+                //calculate the distance of moving
+                var dist = $(".new_platform").eq(num+1).offset().left-w;
+                if((len+10)<dist||len>dist+w){
+                    //fall
                     $(".man img").addClass("fall");
                     setTimeout(function () {
+                        var msg="Game over! Your score is "+score;
+                        alert(msg);
                         clear();
                     }, 500);
                 }else{
-                    $(".man").find("img").attr("src","imgs/stand.png").css("left","0").hide();
+                    $(".man").find("img").attr("src","imgs/man.png").css("left","0").hide();
                     $(".rod").removeClass("fall").width("0");
                     var next=$(".new_platform").eq(num+1).css("left");
                     $(".platform").animate({"left":"-"+next},500,function () {
                         $(".man").find("img").show();
-                        stop=true;
-                        num++;
-                        score+=level;
-                        if(num%5 === 0){
-                            level+=1;
+                            stop=true;
+                            num++;
+                            score+=level;
+                            if(num%5 === 0){
+                                level+=1;
                         }
                         //console.log(score);
                         update();
+                        running = false;
                     })
                 }
             })
@@ -110,19 +124,23 @@ $(document).ready(function (){
     function clear() {
         $(".platform").empty();
         stop = true;
+        running = false;
         num=0;
+        //update the score
         if(score>highest_score)
             highest_score=score;
         rounds+=1;
         //document.cookie="highest_score="+highest_score+";rounds="+rounds;
+        // write to the cookies
         writeCookie("highest_score",highest_score,1);
         writeCookie("rounds",rounds,1);
-        console.log(document.cookie);
+        //console.log(document.cookie);
         score = 0;
-        w = 100;
+        level = 1;
+        width = 100;
         build();
         update();
-        $(".man img").attr("src","imgs/stand.png").css("left","0px").removeClass("fall");
+        $(".man img").attr("src","imgs/man.png").css("left","0px").removeClass("fall");
         $(".rod").removeClass("fall").width("0px");
         $(".platform").css("left","0");
 
